88c4e52eb95c4f6ae14be821a26b301f74c9f0b2
Clifford Bhende <kcbhende@gmail.com>
2018-05-18 12:46:51 +0300

    16/05/2018

--- app/src/main/java/com/example/a16_0128/resourcehub/DashboardActivity.java
+++ app/src/main/java/com/example/a16_0128/resourcehub/DashboardActivity.java
@@ -8,6 +8,9 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class DashboardActivity extends AppCompatActivity implements View.OnClickListener {
@@ -37,6 +40,35 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         pastpapers.setOnClickListener( this);
         events.setOnClickListener( this);
         forum.setOnClickListener( this);
+
+        //spinner
+        final String[] Units = {"Select Unit", "ACS 216","ACS 311", "ACS 312", "ACS 314", "AC 302", "ACS 303"};
+        final String[] Major = {"Select Major","ACS","COM","ACC","ENV"};
+        Spinner Unitspinner = (Spinner) findViewById(R.id.spinList_Units);
+        Spinner Majorspinner = (Spinner) findViewById(R.id.spinList_Major);
+
+        ArrayAdapter<String> majortadpter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Major);
+        ArrayAdapter<String> unitadapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, Units);
+
+        majortadpter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        unitadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Unitspinner.setAdapter(unitadapter);
+        Majorspinner.setAdapter(majortadpter);
+
+        Majorspinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+               // unitSelected.setText(Major [position]);
+               // UnitSelected = unitSelected.getText().toString();
+                //UnitSelected = unitSelected.getText().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     //Card view logic
@@ -55,9 +87,9 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
                 //startActivity(i);
                 break;
             case R.id.btnpastpapers:
-                //i= new Intent(this,course_content.class);
+                i= new Intent(this,PastpapersActivity.class);
                 Toast.makeText(DashboardActivity.this,"Clicked Pastpapers",Toast.LENGTH_SHORT).show();
-                //startActivity(i);
+                startActivity(i);
                 break;
             case R.id.btnassignment:
                 i= new Intent(this,AssignmentActivity.class);
